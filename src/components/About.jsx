5d7ecec9b46c6b282241438ff40f31347b5cd0e7
Gentle-Codes <267931953+Gentle-Codes@users.noreply.github.com>
2026-05-06 17:25:41 +0100

    update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,21 @@ export default function () {
                 src="https://i.postimg.cc/HnB99L1Z/photo-2026-04-20-16-10-25.jpg"
                 alt="my-picture"
                 />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptas voluptatibus corporis ullam distinctio quam esse, molestias id repellendus ut dolore temporibus ab dolorem aut fugit odio nulla minima ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit repellat temporibus magnam, consequuntur quae amet ex voluptate, corrupti ipsam maiores sequi? Quaerat reprehenderit dolore dolor maxime illum, odio obcaecati accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam praesentium adipisci debitis ducimus? Qui et sint, perferendis accusamus exercitationem vel vitae in eius numquam suscipit nihil blanditiis ullam, reiciendis accusantium!</p>
+                <p>
+                    I’m Adeyemi Awotunde, a web developer who loves turning complicated 
+                    stuff into clear, simple solutions. I’m good at mixing logic with visual 
+                    design to craft interfaces that aren’t just functional but also fun and engaging.
+                    <br />
+                    I started out in civil engineering, where every detail counts, so I understand the 
+                    importance of building a solid structure that looks good and serves its purpose. 
+                    That’s what I aim for in web development. I don’t just create good looking UI, I build 
+                    with intention, making sure it’s useful for everyone, whether they’re young or old, 
+                    educated or not.
+                    <br />
+                    When I’m not coding, I’m probably playing (eFootball), which is what I do for fun. 
+                    I also love reading books I believe that "through information, knowledge grows." 
+                    Most of all, I’m a big fan of good music; it’s like my therapy.
+                </p>
             </div>
         </div>
         </section>
